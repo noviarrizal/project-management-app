55eebeb8c7b323c07aae75f35b1352323cf3fcbd
arif noviarrizal <muhammad.noviarrizal@ist.id>
2024-10-29 22:19:16 +0700

    feat: add sidebar

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -1,28 +1,61 @@
 import React from "react";
-import {Search, Settings} from "lucide-react";
+import {MenuIcon, Moon, Search, Settings, Sun} from "lucide-react";
 import Link from "next/link";
 
-const Navbar = () => (
-  <div className="flex items-center justify-between bg-white px-4 py-3 dark:bg-black">
-    <div className="flex items-center gap-8">
-      <div className="relative flex h-min w-[200px]">
-        <Search className="absolute left-[4px] top-1/2 mr-2 h-5 w-5 -translate-y-1/2 transform cursor-pointer dark:text-white" />
-        <input
-          className="w-full rounded border-none bg-gray-100 p-2 pl-8 placeholder-gray-500 focus:border-transparent focus:outline-none dark:bg-gray-700 dark:text-white dark:placeholder-white"
-          placeholder="Search..."
-          type="search"
-        />
+import {useAppDispatch, useAppSelector} from "@/src/lib/redux";
+import {setIsDarkMode, setIsSidebarCollapsed} from "@/src/lib/state";
+
+const Navbar = () => {
+  const dispatch = useAppDispatch();
+  const isSidebarCollapsed = useAppSelector((state) => state.global.isSidebarCollapsed);
+  const isDarkmode = useAppSelector((state) => state.global.isDarkMode);
+
+  return (
+    <div className="flex items-center justify-between bg-white px-4 py-3 dark:bg-black">
+      <div className="flex items-center gap-8">
+        {!isSidebarCollapsed ? null : (
+          <button onClick={() => dispatch(setIsSidebarCollapsed(!isSidebarCollapsed))}>
+            <MenuIcon className="h-8 w-8 cursor-pointer dark:text-white" />
+          </button>
+        )}
+        <div className="relative flex h-min w-[200px]">
+          <Search className="absolute left-[4px] top-1/2 mr-2 h-5 w-5 -translate-y-1/2 transform cursor-pointer dark:text-white" />
+          <input
+            className="w-full rounded border-none bg-gray-100 p-2 pl-8 placeholder-gray-500 focus:border-transparent focus:outline-none dark:bg-gray-700 dark:text-white dark:placeholder-white"
+            placeholder="Search..."
+            type="search"
+          />
+        </div>
+      </div>
+
+      {/* Icons */}
+      <div className="flex items-center">
+        <button
+          className={
+            isDarkmode ? `rounded p-2 dark:hover:bg-gray-700` : `rounded p-2 hover:bg-gray-100`
+          }
+          onClick={() => dispatch(setIsDarkMode(!isDarkmode))}
+        >
+          {isDarkmode ? (
+            <Sun className="h-6 w-6 cursor-pointer dark:text-white" />
+          ) : (
+            <Moon className="h-6 w-6 cursor-pointer dark:text-white" />
+          )}
+        </button>
+        <Link
+          className={
+            isDarkmode
+              ? ` h-min w-min rounded p-2 dark:hover:bg-gray-700`
+              : `h-min w-min rounded p-2 hover:bg-gray-100`
+          }
+          href="/settings"
+        >
+          <Settings className="h-6 w-6 cursor-pointer dark:text-white" />
+        </Link>
+        <div className="ml-2 mr-5 hidden m-h-[2em] w-[0.1rem] bg-gray200 md:inline-block" />
       </div>
     </div>
-
-    {/* Icons */}
-    <div className="flex items-center">
-      <Link className="h-min w-min rounded p-2 hover:bg-gray-100" href="/settings">
-        <Settings className="h-6 w-6 cursor-pointer dark:text-white" />
-      </Link>
-      <div className="ml-2 mr-5 hidden m-h-[2em] w-[0.1rem] bg-gray200 md:inline-block" />
-    </div>
-  </div>
-);
+  );
+};
 
 export default Navbar;
